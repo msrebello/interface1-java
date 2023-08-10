da5158b77ac2234d7fab5a6317582a30fe900749
Matheus Rebello <matheussrb@outlook.com>
2023-08-10 16:21:34 -0300

    Solution with interface

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,11 +1,11 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
 	
 	private static final double TAX_BR_1 = 0.2;
 	private static final double TAX_BR_2 = 0.1;
 	
-	public Double tax(double amount) {
+	public double tax(double amount) {
 	
 		if (amount <= 100) 
 			return amount * TAX_BR_1; 
